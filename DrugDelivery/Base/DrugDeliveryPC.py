f278d1eb513c8e246a121db35faf9de5086b6ee3
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2015-09-04 15:08:49 -0500

    Send a schedule to MCR via GUI

--- DrugDelivery/Base/DrugDeliveryPC.py
+++ DrugDelivery/Base/DrugDeliveryPC.py
@@ -2,6 +2,8 @@
 import sys
 import time
 
+from Tkinter import *
+
 import DrugSchedulerMsg
 from tinyos.message import *
 from tinyos.message.Message import *
@@ -20,19 +22,69 @@ class DrugDeliveryPC:
       print 'Received packet #: ', DrugDeliveryPC.counter, 'remaining: ', msg.get_amount(), '%'
       DrugDeliveryPC.counter += 1
 
-  def send(self):
-    time.sleep(2)
+  def send(self, interval, amount):
+    # time.sleep(2)
     payload = DrugSchedulerMsg.DrugSchedulerMsg()
-    payload.set_amount(15)
-    payload.set_time_interval(3)
+    payload.set_time_interval(interval)
+    payload.set_amount(amount)
     self.mif.sendMsg(self.tos_source, 0xFFFF, payload.get_amType(), 0, payload)
+
+class Application(Frame):
+  def __init__(self, master, motestring):
+    self.dl = DrugDeliveryPC(sys.argv[1])
+    self.root = master
+    Frame.__init__(self, master)
+    self.pack(padx = 10, pady = 20)
+    self.createWidgets()
+
+  def send_schedule(self):
+    interval = int(self.interval.get())
+    amount = int(self.amount.get())
+    self.dl.send(interval, amount)
+
+  def quit(self, event=None):
+    self.root.destroy()
+
+  def createWidgets(self):
+
+    self.createInputArea()
+    self.createInfoArea()
+
+    self.bind_all("<q>", self.quit)
+
+  def createInputArea(self):
+    input_frame = Frame(self, bd=15)
+    label_interval = Label(input_frame, text='Every')
+    self.interval = Entry(input_frame, width=4)
+    label_amount = Label(input_frame, text='seconds')
+    self.amount = Entry(input_frame, width=4)
+    label_perc = Label(input_frame, text='%', padx=5)
+    send = Button(input_frame, text='Send', command=self.send_schedule)
+    label_interval.pack(side=LEFT)
+    self.interval.pack(side=LEFT)
+    label_amount.pack(side=LEFT)
+    self.amount.pack(side=LEFT)
+    label_perc.pack(side=LEFT)
+    send.pack()
+    input_frame.pack()
+
+  def createInfoArea(self):
+    info_frame = Frame(self, bd=15, bg='blue')
+    label = Label(info_frame, text='Information')
+    text = Label(info_frame, text='...', bd=5, bg='gray')
+    label.pack()
+    text.pack()
+    info_frame.pack()
+
 
 def main():
   if '-h' in sys.argv or len(sys.argv) < 2:
     print 'Usage: ...'
     sys.exit()
-  dl = DrugDeliveryPC(sys.argv[1])
-  dl.send()
+  root = Tk()
+  root.title('Scheduler')
+  app = Application(root, sys.argv[1])
+  app.mainloop()
 
 if __name__ == '__main__':
   try:
